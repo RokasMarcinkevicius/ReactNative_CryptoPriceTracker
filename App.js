13bf0fc452f98c6a14e3d06dc7700b816280191b
Rokas <rokas.marcinkevicius@ktu.edu>
2022-05-12 01:01:13 +0300

    Initial mock with Sample Data

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import ListItem from "./components/ListItem";
+
+import { SAMPLE_DATA } from "./assets/data/sampleData";
+
+const ListHeader = () => (
+  <>
+    <View style={styles.titleWrapper}>
+      <Text style={styles.largeTitle}>Markets</Text>
+    </View>
+    <View style={styles.divider} />
+  </>
+);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        keyExtractor={(item) => item.id}
+        data={SAMPLE_DATA}
+        renderItem={({ item }) => (
+          <ListItem
+            name={item.name}
+            symbol={item.symbol}
+            currentPrice={item.current_price}
+            priceChangePercentage24h={item.price_change_percentage_24h}
+            logoUrl={item.image}
+          />
+        )}
+        ListHeaderComponent={<ListHeader />}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  titleWrapper: {
+    marginTop: 40,
+    paddingHorizontal: 16,
+  },
+  largeTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  divider: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: "#A9ABB1",
+    marginHorizontal: 16,
+    marginTop: 16,
   },
 });
